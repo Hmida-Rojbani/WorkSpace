00486d37aacbe3fb61479c3dfc5f97295279a534
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-04-01 11:55:09 +0100

    add gestion show

---
+++ gestion.show/src/main/java/gestion/show/Main.java
@@ -0,0 +1,11 @@
+package gestion.show;
+
+import gestion.show.model.Customer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Customer customer = new Customer("Ali", "1224343");
+		System.out.println(customer);
+	}
+}
---
+++ gestion.show/src/main/java/gestion/show/model/Customer.java
@@ -0,0 +1,19 @@
+package gestion.show.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(of = "name")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+	
+	private String name;
+	private String phone;
+
+}
